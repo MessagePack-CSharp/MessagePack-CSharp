b8c022f4b61f17dcf531f37bcb8170775085983c
neuecc <ils@neue.cc>
2017-02-19 19:56:22 +0900

    add UnityProject

---
+++ src/MessagePack.UnityClient/Assets/Scripts/Tests/SimpleTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using RuntimeUnitTestToolkit;
+using System.Collections;
+
+using System.Collections.Generic;
+using System;
+
+namespace MessagePack.UnityClient.Tests
+{
+    public class SimpleTest
+    {
+        public void Hello()
+        {
+            "OK".Is("OK");
+        }
+    }
+}
---
+++ src/MessagePack.UnityClient/Assets/Scripts/Tests/_Loader.cs
@@ -0,0 +1,19 @@
+﻿using RuntimeUnitTestToolkit;
+using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+using System.Text;
+
+namespace MessagePack.UnityClient.Tests
+{
+    public static class UnitTestLoader
+    {
+        [UnityEngine.RuntimeInitializeOnLoadMethod(UnityEngine.RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Register()
+        {
+            // Register Tests
+            UnitTest.RegisterAllMethods<SimpleTest>();
+        }
+    }
+}
