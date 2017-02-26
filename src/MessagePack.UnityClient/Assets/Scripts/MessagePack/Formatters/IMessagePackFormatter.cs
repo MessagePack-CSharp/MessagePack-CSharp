7b46f1f4f67e7994c441b7355a510350503bbfea
neuecc <ils@neue.cc>
2017-02-26 21:21:42 +0900

    unknown the symlink

--- src/MessagePack.UnityClient/Assets/Scripts/MessagePack/Formatters/IMessagePackFormatter.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace MessagePack.Formatters
-{
-    public interface IMessagePackFormatter<T>
-    {
-        int Serialize(ref byte[] bytes, int offset, T value, IFormatterResolver formatterResolver);
-        T Deserialize(byte[] bytes, int offset, IFormatterResolver formatterResolver, out int readSize);
-    }
-}
