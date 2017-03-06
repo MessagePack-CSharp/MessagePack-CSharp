69fd97b62e712804c3789bc4f8f145e5ba03e6d7
neuecc <ils@neue.cc>
2017-03-06 21:38:08 +0900

    0.7.0

--- nuget/push.bat
+++ nuget/push.bat
@@ -1,5 +1,5 @@
-nuget push MessagePack.0.6.1.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push MessagePack.ImmutableCollection.0.6.1.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push MessagePack.ReactiveProperty.0.6.1.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push MessagePack.UnityShims.0.6.1.nupkg -Source https://www.nuget.org/api/v2/package
-REM nuget push MessagePackAnalyzer.1.2.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push MessagePack.0.7.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push MessagePack.ImmutableCollection.0.7.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push MessagePack.ReactiveProperty.0.7.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push MessagePack.UnityShims.0.7.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push MessagePackAnalyzer.1.3.0.nupkg -Source https://www.nuget.org/api/v2/package
