253af324f8ca80a557309505b98d63e7cc0ccf58
neuecc <ils@neue.cc>
2017-04-05 22:32:42 +0900

    ready for 1.2

--- nuget/push.bat
+++ nuget/push.bat
@@ -1,6 +1,6 @@
-nuget push MessagePack.1.1.2.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push MessagePack.ImmutableCollection.1.1.2.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push MessagePack.ReactiveProperty.1.1.2.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push MessagePack.UnityShims.1.1.2.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push MessagePack.AspNetCoreMvcFormatter.1.1.2.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push MessagePackAnalyzer.1.6.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push MessagePack.1.2.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push MessagePack.ImmutableCollection.1.2.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push MessagePack.ReactiveProperty.1.2.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push MessagePack.UnityShims.1.2.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push MessagePack.AspNetCoreMvcFormatter.1.2.0.nupkg -Source https://www.nuget.org/api/v2/package
+REM nuget push MessagePackAnalyzer.1.6.0.nupkg -Source https://www.nuget.org/api/v2/package
