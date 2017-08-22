3c5f63ad5ae5cc1ea40d61adc975ad7ae17ff01f
neuecc <ils@neue.cc>
2017-08-22 15:45:39 +0900

    starting IOverwriteMessagePackFormatter

--- src/MessagePack/Formatters/IMessagePackFormatter.cs
+++ src/MessagePack/Formatters/IMessagePackFormatter.cs
@@ -6,4 +6,25 @@ namespace MessagePack.Formatters
         int Serialize(ref byte[] bytes, int offset, T value, IFormatterResolver formatterResolver);
         T Deserialize(byte[] bytes, int offset, IFormatterResolver formatterResolver, out int readSize);
     }
+
+    public interface IOverwriteMessagePackFormatter<T> : IMessagePackFormatter<T>
+    {
+        void DeserializeTo(ref T to, byte[] bytes, int offset, IFormatterResolver formatterResolver, out int readSize);
+    }
+
+    public static class IMessagePackFormatterExtensions
+    {
+        public static void DeserializeTo<T>(this IMessagePackFormatter<T> formatter, ref T to, byte[] bytes, int offset, IFormatterResolver formatterResolver, out int readSize)
+        {
+            var overwriteFormatter = formatter as IOverwriteMessagePackFormatter<T>;
+            if (overwriteFormatter != null)
+            {
+                overwriteFormatter.DeserializeTo(ref to, bytes, offset, formatterResolver, out readSize);
+            }
+            else
+            {
+                to = formatter.Deserialize(bytes, offset, formatterResolver, out readSize);
+            }
+        }
+    }
 }
---
+++ src/MessagePack/MessagePackConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using MessagePack.Formatters;
+using System;
+
+namespace MessagePack
+{
+    public interface IResolverConfiguration
+    {
+        CollectionDeserializeToBehaviour CollectionDeserializeToBehaviour { get; }
+    }
+
+    public enum CollectionDeserializeToBehaviour
+    {
+        Add,
+        Overwrite,
+    }
+
+    public class MessagePackConfiguration : IResolverConfiguration, IMessagePackFormatter<IResolverConfiguration>
+    {
+        public static readonly MessagePackConfiguration Default = new MessagePackConfiguration(CollectionDeserializeToBehaviour.Add);
+
+        public CollectionDeserializeToBehaviour CollectionDeserializeToBehaviour { get; private set; }
+
+        public MessagePackConfiguration(CollectionDeserializeToBehaviour collectionDeserializeToBehaviour)
+        {
+            this.CollectionDeserializeToBehaviour = collectionDeserializeToBehaviour;
+        }
+
+        int IMessagePackFormatter<IResolverConfiguration>.Serialize(ref byte[] bytes, int offset, IResolverConfiguration value, IFormatterResolver formatterResolver)
+        {
+            throw new NotSupportedException("IMessagePackFormatter method is dummy, please call AsConfiguration().");
+        }
+
+        IResolverConfiguration IMessagePackFormatter<IResolverConfiguration>.Deserialize(byte[] bytes, int offset, IFormatterResolver formatterResolver, out int readSize)
+        {
+            throw new NotSupportedException("IMessagePackFormatter method is dummy, please call AsConfiguration().");
+        }
+    }
+
+    public static class ResolverConfigurationExtensions
+    {
+        public static IResolverConfiguration GetConfiguration(this IFormatterResolver formatterResolver)
+        {
+            var configuration = formatterResolver.GetFormatter<IResolverConfiguration>();
+            return (configuration == null) ? MessagePackConfiguration.Default : configuration.AsConfiguration();
+        }
+
+        public static IResolverConfiguration AsConfiguration(this IMessagePackFormatter<IResolverConfiguration> configurationFormatter)
+        {
+            return (IResolverConfiguration)configurationFormatter;
+        }
+    }
+}
