039790b0990fc27a2ee95c662866f82c685e6ee6
neuecc <ils@neue.cc>
2017-08-30 23:43:57 +0900

    small improvement of ByteArrayComparer.Equals

--- src/MessagePack/Internal/ByteArrayComparer.cs
+++ src/MessagePack/Internal/ByteArrayComparer.cs
@@ -45,51 +45,54 @@ namespace MessagePack.Internal
                 return false;
             }
 
-            fixed (byte* p1 = &xs[0])
-            fixed (byte* p2 = &ys[0])
+            fixed (byte* p1 = &xs[xsOffset])
+            fixed (byte* p2 = &ys[ysOffset])
             {
-                var x1 = p1 + xsOffset;
-                var x2 = p2 + ysOffset;
-
-                var length = xsCount;
-                var loooCount = length / 8;
-
-                for (var i = 0; i < loooCount; i++, x1 += 8, x2 += 8)
+                switch (xsCount)
                 {
-                    if (*(long*)x1 != *(long*)x2)
-                    {
-                        return false;
-                    }
-                }
+                    case 0:
+                        return true;
+                    case 1:
+                        return *p1 == *p2;
+                    case 2:
+                        return *(short*)p1 == *(short*)p2;
+                    case 3:
+                        if (*(byte*)p1 != *(byte*)p2) return false;
+                        return *(short*)(p1 + 1) == *(short*)(p2 + 1);
+                    case 4:
+                        return *(int*)p1 == *(int*)p2;
+                    case 5:
+                        if (*(byte*)p1 != *(byte*)p2) return false;
+                        return *(int*)(p1 + 1) == *(int*)(p2 + 1);
+                    case 6:
+                        if (*(short*)p1 != *(short*)p2) return false;
+                        return *(int*)(p1 + 2) == *(int*)(p2 + 2);
+                    case 7:
+                        if (*(byte*)p1 != *(byte*)p2) return false;
+                        if (*(short*)(p1 + 1) != *(short*)(p2 + 1)) return false;
+                        return *(int*)(p1 + 3) == *(int*)(p2 + 3);
+                    default:
+                        {
+                            var x1 = p1;
+                            var x2 = p2;
 
-                if ((length & 4) != 0)
-                {
-                    if (*(int*)x1 != *(int*)x2)
-                    {
-                        return false;
-                    }
-                    x1 += 4;
-                    x2 += 4;
-                }
+                            byte* xEnd = p1 + xsCount - 8;
+                            byte* yEnd = p2 + ysCount - 8;
 
-                if ((length & 2) != 0)
-                {
-                    if (*(short*)x1 != *(short*)x2)
-                    {
-                        return false;
-                    }
-                    x1 += 2;
-                    x2 += 2;
-                }
+                            while (x1 < xEnd)
+                            {
+                                if (*(long*)x1 != *(long*)x2)
+                                {
+                                    return false;
+                                }
 
-                if ((length & 1) != 0)
-                {
-                    if (*x1 != *x2)
-                    {
-                        return false;
-                    }
+                                x1 += 8;
+                                x2 += 8;
+                            }
+
+                            return *(long*)xEnd == *(long*)yEnd;
+                        }
                 }
-                return true;
             }
         }
 
