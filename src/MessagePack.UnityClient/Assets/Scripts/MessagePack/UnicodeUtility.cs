1bec0295403d46860d74dda2ddbfa52ec90adff4
itn3000 <itn3000@gmail.com>
2017-10-04 04:11:16 +0900

    add unity settings(only passes to Encoding.UTF8.GetString)

---
+++ src/MessagePack.UnityClient/Assets/Scripts/MessagePack/UnicodeUtility.cs
@@ -0,0 +1,25 @@
+namespace MessagePack.Internal
+{
+    using System;
+    using System.Text;
+    /// <summary>
+    /// unicode manupilation utilities
+    /// </summary>
+    internal static class UnicodeUtility
+    {
+        [ThreadStatic]
+        static char[] Utf8Buffer;
+        /// <summary>
+        /// convert utf8 bytearray to string instance
+        /// </summary>
+        /// <param name="data">UTF-8 byte array(without BOM)</param>
+        /// <param name="offset">start position in data</param>
+        /// <param name="count">number of byte length</param>
+        /// <remarks>for unity, this function passes arguments to Encoding.UTF8.GetBytes<remarks>
+        /// <returns>converted string instance</returns>
+        public static unsafe string GetStringFromUtf8(byte[] data, int offset, int count)
+        {
+            return Encoding.UTF8.GetString(data, offset, count);
+        }
+    }
+}
