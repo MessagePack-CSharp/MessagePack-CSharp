1ee3d54aa3453ebb00b6b1f5b3c3dcb2e9aeed22
Andrew Arnott <andrewarnott@gmail.com>
2019-03-15 11:19:57 -0700

    Fix build breaks after merge

--- benchmark/SerializerBenchmark/Serializers/MessagePackSerializer.cs
+++ benchmark/SerializerBenchmark/Serializers/MessagePackSerializer.cs
@@ -20,12 +20,12 @@ public class MessagePack_v2 : SerializerBase
 {
     public override T Deserialize<T>(object input)
     {
-        return newmsgpack::MessagePack.MessagePackSerializer.Deserialize<T>((byte[])input);
+        return new newmsgpack::MessagePack.MessagePackSerializer().Deserialize<T>((byte[])input);
     }
 
     public override object Serialize<T>(T input)
     {
-        return newmsgpack::MessagePack.MessagePackSerializer.Serialize<T>(input);
+        return new newmsgpack::MessagePack.MessagePackSerializer().Serialize<T>(input);
     }
 }
 
@@ -46,11 +46,11 @@ public class MessagePackLz4_v2 : SerializerBase
 {
     public override T Deserialize<T>(object input)
     {
-        return newmsgpack::MessagePack.LZ4MessagePackSerializer.Deserialize<T>((byte[])input);
+        return new newmsgpack::MessagePack.LZ4MessagePackSerializer().Deserialize<T>((byte[])input);
     }
 
     public override object Serialize<T>(T input)
     {
-        return newmsgpack::MessagePack.LZ4MessagePackSerializer.Serialize<T>(input);
+        return new newmsgpack::MessagePack.LZ4MessagePackSerializer().Serialize<T>(input);
     }
 }
