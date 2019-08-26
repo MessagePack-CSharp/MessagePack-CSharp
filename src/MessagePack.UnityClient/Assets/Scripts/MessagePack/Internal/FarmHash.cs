00ca6c9701adaf2c699bf1752ab8c506376d01b1
neuecc <ils@neue.cc>
2019-08-26 12:51:34 +0900

    remove ByteArrayComparer

--- src/MessagePack.UnityClient/Assets/Scripts/MessagePack/Internal/FarmHash.cs
+++ src/MessagePack.UnityClient/Assets/Scripts/MessagePack/Internal/FarmHash.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) All contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#if !UNITY_2018_3_OR_NEWER
-
 using System;
 using System.Runtime.CompilerServices;
 
@@ -619,5 +617,3 @@ namespace MessagePack.Internal
         #endregion
     }
 }
-
-#endif
