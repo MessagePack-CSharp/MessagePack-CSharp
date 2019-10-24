ce3e38cfb70931920e346a83d2c3887887ca36b5
Andrew Arnott <andrewarnott@gmail.com>
2019-10-24 12:20:45 -0700

    Provide SerializationState to IFormatterResolver as well

--- src/MessagePack.UnityClient/Assets/Scripts/MessagePack/IFormatterResolver.cs
+++ src/MessagePack.UnityClient/Assets/Scripts/MessagePack/IFormatterResolver.cs
@@ -12,18 +12,30 @@ namespace MessagePack
 {
     public interface IFormatterResolver
     {
-        IMessagePackFormatter<T> GetFormatter<T>();
+        /// <summary>
+        /// Gets an <see cref="IMessagePackFormatter{T}"/> instance that can serialize or deserialize some type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of value to be serialized or deserialized.</typeparam>
+        /// <param name="state">State associated with this particular serialization or deserialization operation.</param>
+        /// <returns>A formatter, if this resolver supplies one for type <typeparamref name="T"/>; otherwise <c>null</c>.</returns>
+        IMessagePackFormatter<T> GetFormatter<T>(ref SerializationState state);
     }
 
     public static class FormatterResolverExtensions
     {
+        private static readonly Type[] DelegateCacheTypeArray = new Type[1];
+
+        private static readonly Type[] GetFormatterParameterTypes = new Type[] { typeof(IFormatterResolver), typeof(SerializationState).MakeByRefType() };
+
+        private delegate object GetFormatterDelegate(IFormatterResolver resolver, ref SerializationState state);
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IMessagePackFormatter<T> GetFormatterWithVerify<T>(this IFormatterResolver resolver)
+        public static IMessagePackFormatter<T> GetFormatterWithVerify<T>(this IFormatterResolver resolver, ref SerializationState state)
         {
             IMessagePackFormatter<T> formatter;
             try
             {
-                formatter = resolver.GetFormatter<T>();
+                formatter = resolver.GetFormatter<T>(ref state);
             }
             catch (TypeInitializationException ex)
             {
@@ -52,12 +64,25 @@ namespace MessagePack
             throw new FormatterNotRegisteredException(t.FullName + " is not registered in this resolver. resolver:" + resolver.GetType().Name);
         }
 
-        public static object GetFormatterDynamic(this IFormatterResolver resolver, Type type)
+        public static object GetFormatterDynamic(this IFormatterResolver resolver, Type type, ref SerializationState state)
         {
-            MethodInfo methodInfo = typeof(IFormatterResolver).GetRuntimeMethod(nameof(IFormatterResolver.GetFormatter), Type.EmptyTypes);
-
-            var formatter = methodInfo.MakeGenericMethod(type).Invoke(resolver, null);
+            var cache = typeof(DelegateCache<>).MakeGenericType(type);
+            var del = (GetFormatterDelegate)cache.GetField(nameof(DelegateCache<int>.MyDelegate)).GetValue(null);
+            var formatter = del(resolver, ref state);
             return formatter;
+        }
+
+        private static class DelegateCache<T>
+        {
+            internal static readonly GetFormatterDelegate MyDelegate;
+
+            static DelegateCache()
+            {
+                MethodInfo methodInfo = typeof(DelegateCache<T>).GetRuntimeMethod(nameof(GetFormatterDynamic), GetFormatterParameterTypes);
+                MyDelegate = (GetFormatterDelegate)methodInfo.CreateDelegate(typeof(GetFormatterDelegate));
+            }
+
+            private static object GetFormatterDynamic(IFormatterResolver resolver, ref SerializationState state) => resolver.GetFormatter<T>(ref state);
         }
     }
 
