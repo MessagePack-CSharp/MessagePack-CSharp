a86591ecdb24c961e277a08c7ed4e6c63bd4910a
Andrew Arnott <andrewarnott@gmail.com>
2019-11-05 22:50:12 -0700

    Bring Nerdbank.Streams dependency back
    This at least gets the Unity build working again.

--- src/MessagePack.UnityClient/copy_assets.sh
+++ src/MessagePack.UnityClient/copy_assets.sh
@@ -18,6 +18,7 @@ if ! [[ -d "${SCRIPT_DIR}/Assets/Plugins" ]] ; then
   mkdir -p ${SCRIPT_DIR}/Assets/Plugins
 fi
 
+cp ${SCRIPT_DIR}/../../bin/MessagePack/${BUILDCONFIGURATION}/netstandard2.0/publish/Nerdbank.Streams.dll ${SCRIPT_DIR}/Assets/Plugins/Nerdbank.Streams.dll
 cp ${SCRIPT_DIR}/../../bin/MessagePack/${BUILDCONFIGURATION}/netstandard2.0/publish/System.Buffers.dll ${SCRIPT_DIR}/Assets/Plugins/System.Buffers.dll
 cp ${SCRIPT_DIR}/../../bin/MessagePack/${BUILDCONFIGURATION}/netstandard2.0/publish/System.Memory.dll ${SCRIPT_DIR}/Assets/Plugins/System.Memory.dll
 cp ${SCRIPT_DIR}/../../bin/MessagePack/${BUILDCONFIGURATION}/netstandard2.0/publish/System.Runtime.CompilerServices.Unsafe.dll ${SCRIPT_DIR}/Assets/Plugins/System.Runtime.CompilerServices.Unsafe.dll
