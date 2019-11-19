a81989f414b53f6ce91e169aa909643f0487a465
Andrew Arnott <andrewarnott@gmail.com>
2019-11-19 15:28:05 -0700

    Reduce number of build tasks
    This moves some of the logic from YML to ps1 scripts where it can be reproduced locally and therefore is more diagnosable and maintainable.
    
    It also adds an init script to the repo root which installs whatever .NET Core SDK and runtimes are required to build and run tests in this repo.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,8 +1,26 @@
 # Contributing
 
+## Dependencies
+
+* [Visual Studio 2019](https://visualstudio.microsoft.com/)
+* [Unity Editor](https://unity3d.com/unity/editor) (optional)
+* .NET Core SDK and runtimes (run `init` to install)
+
+This repo uses the .NET Core SDK and runtimes to build and test the projects.
+You can install the right versions of the SDK and runtimes by running the `init.ps1` script at the root of the repo.
+
+By default no elevation is required as these toolsets are installed in a per-user directory. Launching `devenv` from the same PowerShell window that you ran the script will lead VS to discover these per-user toolsets.
+To get VS to find the toolsets when launched from the Start Menu, run `init -InstallLocality machine`, which requires elevation for each SDK or runtime installed.
+
 ## How to Build
 
 Open `MessagePack.sln` on Visual Studio 2019.
+
+Alternatively you may build from the command line using `msbuild.exe` or:
+
+    dotnet build /p:platform=NoVSIX
+
+## Unity
 
 Unity Project requires several dependency DLL's. At first, run `copy_assets.bat` under `src\MessagePack.UnityClient`.
 Then open that directory in the Unity Editor.
---
+++ init.ps1
@@ -0,0 +1,61 @@
+<#
+.SYNOPSIS
+Installs dependencies required to build and test the projects in this repository.
+.DESCRIPTION
+This MAY not require elevation, as the SDK and runtimes are installed locally to this repo location,
+unless `-InstallLocality machine` is specified.
+.PARAMETER InstallLocality
+A value indicating whether dependencies should be installed locally to the repo or at a per-user location.
+Per-user allows sharing the installed dependencies across repositories and allows use of a shared expanded package cache.
+Visual Studio will only notice and use these SDKs/runtimes if VS is launched from the environment that runs this script.
+Per-repo allows for high isolation, allowing for a more precise recreation of the environment within an Azure Pipelines build.
+When using 'repo', environment variables are set to cause the locally installed dotnet SDK to be used.
+Per-repo can lead to file locking issues when dotnet.exe is left running as a build server and can be mitigated by running `dotnet build-server shutdown`.
+Per-machine requires elevation and will download and install all SDKs and runtimes to machine-wide locations so all applications can find it.
+.PARAMETER NoPrerequisites
+Skips the installation of prerequisite software (e.g. SDKs, tools).
+.PARAMETER NoRestore
+Skips the package restore step.
+.PARAMETER AccessToken
+An optional access token for authenticating to Azure Artifacts authenticated feeds.
+#>
+[CmdletBinding(SupportsShouldProcess=$true)]
+Param (
+    [ValidateSet('repo','user','machine')]
+    [string]$InstallLocality='user',
+    [Parameter()]
+    [switch]$NoPrerequisites,
+    [Parameter()]
+    [switch]$NoRestore,
+    [Parameter()]
+    [string]$AccessToken
+)
+
+if (!$NoPrerequisites) {
+    & "$PSScriptRoot\tools\Install-NuGetCredProvider.ps1" -AccessToken $AccessToken
+    & "$PSScriptRoot\tools\Install-DotNetSdk.ps1" -InstallLocality $InstallLocality
+}
+
+# Workaround nuget credential provider bug that causes very unreliable package restores on Azure Pipelines
+$env:NUGET_PLUGIN_HANDSHAKE_TIMEOUT_IN_SECONDS=20
+$env:NUGET_PLUGIN_REQUEST_TIMEOUT_IN_SECONDS=20
+
+Push-Location $PSScriptRoot
+try {
+    $HeaderColor = 'Green'
+
+    if (!$NoRestore) {
+        Write-Host "Restoring NuGet packages" -ForegroundColor $HeaderColor
+        dotnet restore
+        if ($lastexitcode -ne 0) {
+            throw "Failure while restoring packages."
+        }
+    }
+}
+catch {
+    Write-Error $error[0]
+    exit $lastexitcode
+}
+finally {
+    Pop-Location
+}
