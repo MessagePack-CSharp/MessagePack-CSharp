3dc41889766c8f1b18c0a5e7904242fbdf6205cd
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-03-18 19:57:08 +0900

    Rewrite the header copyright

--- src/MessagePack.UnityClient/Assets/Scripts/MessagePack/Unity/MSPack/MSPackMonoCecilInstaller.cs
+++ src/MessagePack.UnityClient/Assets/Scripts/MessagePack/Unity/MSPack/MSPackMonoCecilInstaller.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) pCYSl5EDgo. All rights reserved.
+﻿// Copyright (c) All contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #if UNITY_EDITOR
