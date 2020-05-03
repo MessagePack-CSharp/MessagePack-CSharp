1586a4d4c37e95a89b50edf5bc5e71bf4fb00c88
Andrew Arnott <andrewarnott@gmail.com>
2020-05-03 17:48:18 -0600

    Build mpc as a dotnet tool

--- azure-pipelines/artifacts/mpc.ps1
+++ azure-pipelines/artifacts/mpc.ps1
@@ -4,16 +4,8 @@ if (!$BuildConfiguration) {
     $BuildConfiguration = 'Debug'
 }
 
-$ArtifactBasePath = "$RepoRoot\obj\_artifacts"
-$mpcArtifactPath = "$ArtifactBasePath\mpc"
-if (-not (Test-Path "$mpcArtifactPath/linux")) { New-Item -ItemType Directory -Path "$mpcArtifactPath/linux" | Out-Null }
-if (-not (Test-Path "$mpcArtifactPath/osx")) { New-Item -ItemType Directory -Path "$mpcArtifactPath/osx" | Out-Null }
-if (-not (Test-Path "$mpcArtifactPath/win")) { New-Item -ItemType Directory -Path "$mpcArtifactPath/win" | Out-Null }
-
-Copy-Item "$RepoRoot/bin/MessagePack.Generator/$BuildConfiguration/netcoreapp3.0/linux-x64/publish/*" "$mpcArtifactPath/linux" -Container
-Copy-Item "$RepoRoot/bin/MessagePack.Generator/$BuildConfiguration/netcoreapp3.0/osx-x64/publish/*" "$mpcArtifactPath/osx" -Container
-Copy-Item "$RepoRoot/bin/MessagePack.Generator/$BuildConfiguration/netcoreapp3.0/win-x64/publish/*" "$mpcArtifactPath/win" -Container
+$mpcArtifactPath = "$RepoRoot/bin/MessagePack.Generator/$BuildConfiguration/netcoreapp3.0"
 
 @{
-    "$mpcArtifactPath" = (Get-ChildItem $mpcArtifactPath -Recurse);
+    $mpcArtifactPath = (Get-ChildItem $mpcArtifactPath -Recurse);
 }
