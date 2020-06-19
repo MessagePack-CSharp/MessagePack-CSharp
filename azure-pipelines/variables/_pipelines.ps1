90a2b1a896dbaab81d58a56c6722aa8d9d705390
Andrew Arnott <andrewarnott@gmail.com>
2020-06-19 05:26:55 -0600

    Apply latest Library.Template scripts
    This comes from https://github.com/AArnott/Library.Template.git commit c4a24d10ecf2d391cdd9d89c2652114c695f6db6

--- azure-pipelines/variables/_pipelines.ps1
+++ azure-pipelines/variables/_pipelines.ps1
@@ -5,11 +5,17 @@
 # what the build would do. So only set them if they have not already been set.
 
 (& "$PSScriptRoot\_all.ps1").GetEnumerator() |% {
-    if (Test-Path -Path "env:$($_.Key.ToUpper())") {
-        Write-Host "Skipping setting $($_.Key) because variable is already set." -ForegroundColor Cyan
+    # Always use ALL CAPS for env var names since Azure Pipelines converts variable names to all caps and on non-Windows OS, env vars are case sensitive.
+    $keyCaps = $_.Key.ToUpper()
+    if (Test-Path -Path "env:$keyCaps") {
+        Write-Host "Skipping setting $keyCaps because variable is already set." -ForegroundColor Cyan
     } else {
-        Write-Host "$($_.Key)=$($_.Value)" -ForegroundColor Yellow
-        Write-Host "##vso[task.setvariable variable=$($_.Key);]$($_.Value)"
-        Set-Item -Path "env:$($_.Key)" -Value $_.Value
+        Write-Host "$keyCaps=$($_.Value)" -ForegroundColor Yellow
+        if ($env:TF_BUILD) {
+            Write-Host "##vso[task.setvariable variable=$keyCaps;]$($_.Value)"
+        } elseif ($env:GITHUB_ACTIONS) {
+            Write-Host "::set-env name=$keyCaps::$($_.Value)"
+        }
+        Set-Item -Path "env:$keyCaps" -Value $_.Value
     }
 }
