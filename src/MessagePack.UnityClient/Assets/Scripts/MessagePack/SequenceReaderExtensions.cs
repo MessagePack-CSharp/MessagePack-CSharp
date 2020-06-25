04c8858103ee313582d2c980877ff233dcb4078b
Andrew Arnott <andrewarnott@gmail.com>
2020-06-25 11:29:06 -0600

    Move SequenceReader into our own namespace
    It is no longer replaceable by what is included in netcoreapp3.1

--- src/MessagePack.UnityClient/Assets/Scripts/MessagePack/SequenceReaderExtensions.cs
+++ src/MessagePack.UnityClient/Assets/Scripts/MessagePack/SequenceReaderExtensions.cs
@@ -5,12 +5,13 @@
  * The .NET Foundation licenses this file to you under the MIT license.
  * See the LICENSE file in the project root for more information. */
 
+using System;
 using System.Buffers.Binary;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-namespace System.Buffers
+namespace MessagePack
 {
     internal static partial class SequenceReaderExtensions
     {
