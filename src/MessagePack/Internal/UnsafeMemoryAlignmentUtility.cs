1979e3138ae721c83333612043cfad6d0827e82f
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-08-06 13:53:58 +0900

    Add Alignment of 32

--- src/MessagePack/Internal/UnsafeMemoryAlignmentUtility.cs
+++ src/MessagePack/Internal/UnsafeMemoryAlignmentUtility.cs
@@ -16,5 +16,13 @@ namespace MessagePack.Internal
             difference = (16 - difference) & 0xfUL;
             return (int)difference;
         }
+
+        public static int CalculateDifferenceAlign32(void* pointer)
+        {
+            var difference = new UIntPtr(pointer).ToUInt64();
+            difference -= (difference >> 5) << 5;
+            difference = (32 - difference) & 0x1fUL;
+            return (int)difference;
+        }
     }
 }
