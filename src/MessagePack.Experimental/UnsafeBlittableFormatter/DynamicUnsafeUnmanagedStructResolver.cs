13ef878156425e5e8b7a0e0030ed937571bbcc60
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-09-22 19:13:34 +0900

    Add FormatterRegister
    This (de)serializes struct T where T : unmanaged directly and unsafely.

---
+++ src/MessagePack.Experimental/UnsafeBlittableFormatter/DynamicUnsafeUnmanagedStructResolver.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) All contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using MessagePack.Formatters;
+
+namespace MessagePack.Resolvers
+{
+    public sealed class DynamicUnsafeUnmanagedStructResolver : IFormatterResolver
+    {
+        public static readonly DynamicUnsafeUnmanagedStructResolver Instance = new DynamicUnsafeUnmanagedStructResolver();
+
+        private static readonly MethodInfo baseIsReferenceOrContainsReferences = typeof(RuntimeHelpers).GetMethod(nameof(RuntimeHelpers.IsReferenceOrContainsReferences)) ?? throw new NullReferenceException();
+        private static readonly Type baseFormatterCache = typeof(FormatterCache<>);
+        private static readonly Type baseStruct = typeof(UnsafeUnmanagedStructFormatter<>);
+        private static readonly Type baseArray = typeof(UnsafeUnmanagedStructArrayFormatter<>);
+
+        [ThreadStatic] private static Type[]? one;
+
+        static DynamicUnsafeUnmanagedStructResolver()
+        {
+            var currentDomain = AppDomain.CurrentDomain;
+            var assemblies = currentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                ProcessAssembly(assembly);
+            }
+
+            currentDomain.AssemblyLoad += CurrentDomainOnAssemblyLoad;
+        }
+
+        private DynamicUnsafeUnmanagedStructResolver()
+        {
+        }
+
+        private static void CurrentDomainOnAssemblyLoad(object? sender, AssemblyLoadEventArgs args)
+        {
+            ProcessAssembly(args.LoadedAssembly);
+        }
+
+        private static void ProcessAssembly(Assembly assembly)
+        {
+            one ??= new Type[1];
+
+            foreach (var module in assembly.Modules)
+            {
+                foreach (var attribute in module.GetCustomAttributes<UnsafeUnmanagedStructAttribute>())
+                {
+                    if (attribute.Kind == 0)
+                    {
+                        continue;
+                    }
+
+                    var type = attribute.Type;
+                    one[0] = type;
+                    var isReferenceOrContainsReferences = (bool)baseIsReferenceOrContainsReferences.MakeGenericMethod(one).Invoke(null, null)!;
+                    if (isReferenceOrContainsReferences)
+                    {
+                        throw new MessagePackSerializationException("UnsafeUnmanagedStructAttribute target type should be unmanaged type! type: " + type);
+                    }
+
+                    if ((attribute.Kind & UnsafeUnmanagedStructFormatterImplementationKind.Self) != 0)
+                    {
+                        var instance = baseStruct.MakeGenericType(one).GetField("Instance")!.GetValue(default);
+                        baseFormatterCache.MakeGenericType(one).GetField("Formatter")!.SetValue(null, instance);
+                    }
+
+                    if ((attribute.Kind & UnsafeUnmanagedStructFormatterImplementationKind.Array) != 0)
+                    {
+                        var instance = baseArray.MakeGenericType(one).GetField("Instance")!.GetValue(default);
+                        one[0] = type.MakeArrayType();
+                        baseFormatterCache.MakeGenericType(one).GetField("Formatter")!.SetValue(null, instance);
+                    }
+                }
+            }
+        }
+
+        public IMessagePackFormatter<T>? GetFormatter<T>()
+        {
+            return FormatterCache<T>.Formatter;
+        }
+
+        private static class FormatterCache<T>
+        {
+            public static IMessagePackFormatter<T>? Formatter;
+        }
+    }
+}
---
+++ src/MessagePack.Experimental/UnsafeBlittableFormatter/UnsafeUnmanagedStructAttribute.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) All contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace MessagePack
+{
+    [AttributeUsage(AttributeTargets.Module, AllowMultiple = true)]
+    public sealed class UnsafeUnmanagedStructAttribute : Attribute
+    {
+        public Type Type { get; }
+
+        public UnsafeUnmanagedStructFormatterImplementationKind Kind { get; }
+
+        public UnsafeUnmanagedStructAttribute(Type type, UnsafeUnmanagedStructFormatterImplementationKind kind)
+        {
+            Type = type;
+            Kind = kind;
+        }
+    }
+
+    [Flags]
+    public enum UnsafeUnmanagedStructFormatterImplementationKind
+    {
+        Self = 1,
+        Array = 2,
+    }
+}
