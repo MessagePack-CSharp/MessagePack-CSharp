07adf91498bca3788cef16857b8ae23190b22d2f
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-09-27 16:46:10 +0900

    Add twin test

---
+++ tests/MessagePack.Experimental.Tests/CircularReference/TwinExample.cs
@@ -0,0 +1,97 @@
+﻿// Copyright (c) All contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#pragma warning disable SA1649 // File name should match first type name
+
+namespace MessagePack.Experimental.Tests.CircularReference
+{
+    [MessagePackObject]
+    public class TwinExample0
+    {
+        [Key(0)]
+        public string? Name { get; set; }
+
+        [Key(1)]
+        public TwinExample1? Partner { get; set; }
+    }
+
+    [MessagePackObject]
+    public class TwinExample1
+    {
+        [Key(0)]
+        public string? Name { get; set; }
+
+        [Key(1)]
+        public TwinExample0? Partner { get; set; }
+    }
+
+    public sealed class TwinExample0Overwriter : IMessagePackDeserializeOverwriter<TwinExample0>
+    {
+        public static readonly TwinExample0Overwriter Instance = new TwinExample0Overwriter();
+
+        private TwinExample0Overwriter()
+        {
+        }
+
+        public void DeserializeOverwrite(ref MessagePackReader reader, MessagePackSerializerOptions options, TwinExample0 value)
+        {
+            var length = reader.ReadArrayHeader();
+            if (length == 0)
+            {
+                return;
+            }
+
+            var resolver = options.Resolver;
+            for (var i = 0; i < length; i++)
+            {
+                switch (i)
+                {
+                    default:
+                        reader.Skip();
+                        break;
+                    case 0:
+                        value.Name = resolver.GetFormatterWithVerify<string>().Deserialize(ref reader, options);
+                        break;
+                    case 1:
+                        value.Partner = resolver.GetFormatterWithVerify<TwinExample1>().Deserialize(ref reader, options);
+                        break;
+                }
+            }
+        }
+    }
+
+    public sealed class TwinExample1Overwriter : IMessagePackDeserializeOverwriter<TwinExample1>
+    {
+        public static readonly TwinExample1Overwriter Instance = new TwinExample1Overwriter();
+
+        private TwinExample1Overwriter()
+        {
+        }
+
+        public void DeserializeOverwrite(ref MessagePackReader reader, MessagePackSerializerOptions options, TwinExample1 value)
+        {
+            var length = reader.ReadArrayHeader();
+            if (length == 0)
+            {
+                return;
+            }
+
+            var resolver = options.Resolver;
+            for (var i = 0; i < length; i++)
+            {
+                switch (i)
+                {
+                    default:
+                        reader.Skip();
+                        break;
+                    case 0:
+                        value.Name = resolver.GetFormatterWithVerify<string>().Deserialize(ref reader, options);
+                        break;
+                    case 1:
+                        value.Partner = resolver.GetFormatterWithVerify<TwinExample0>().Deserialize(ref reader, options);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ tests/MessagePack.Experimental.Tests/CircularReference/TwinTest.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) All contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using MessagePack.Formatters;
+using MessagePack.Resolvers;
+using NUnit.Framework;
+
+namespace MessagePack.Experimental.Tests.CircularReference
+{
+    public class TwinTest
+    {
+        private MessagePackSerializerOptions options;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var resolver = CompositeResolver.Create(
+                new IMessagePackFormatter[]
+                {
+                    new CircularFormatter<TwinExample0>(StandardResolver.Instance.GetFormatterWithVerify<TwinExample0>()!, TwinExample0Overwriter.Instance),
+                    new CircularFormatter<TwinExample1>(StandardResolver.Instance.GetFormatterWithVerify<TwinExample1>()!, TwinExample1Overwriter.Instance),
+                },
+                new[] { StandardResolver.Instance });
+            options = MessagePackSerializerOptions.Standard.WithResolver(resolver);
+        }
+
+        [Test]
+        public void NullTest0()
+        {
+            var original = default(TwinExample0);
+
+            var binary = MessagePackSerializer.Serialize(original, options);
+            var result = MessagePackSerializer.Deserialize<TwinExample0>(binary, options);
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void NullTest1()
+        {
+            var original = default(TwinExample1);
+
+            var binary = MessagePackSerializer.Serialize(original, options);
+            var result = MessagePackSerializer.Deserialize<TwinExample1>(binary, options);
+
+            Assert.IsNull(result);
+        }
+
+        [TestCase("")]
+        [TestCase("名無しの権兵衛")]
+        public void FieldNullTest0(string name)
+        {
+            var original = new TwinExample0() { Name = name };
+
+            var binary = MessagePackSerializer.Serialize(original, options);
+            var result = MessagePackSerializer.Deserialize<TwinExample0>(binary, options);
+
+            Assert.IsNotNull(result);
+            Assert.IsNull(result.Partner);
+            Assert.AreEqual(result.Name, name);
+        }
+
+        [TestCase("")]
+        [TestCase("名無しの権兵衛")]
+        public void FieldNullTest1(string name)
+        {
+            var original = new TwinExample1() { Name = name };
+
+            var binary = MessagePackSerializer.Serialize(original, options);
+            var result = MessagePackSerializer.Deserialize<TwinExample1>(binary, options);
+
+            Assert.IsNotNull(result);
+            Assert.IsNull(result.Partner);
+            Assert.AreEqual(result.Name, name);
+        }
+
+        [TestCase("", "empty")]
+        [TestCase("名無しの権兵衛", "じゅげむじゅげむごこうのすりきれ")]
+        public void MirrorTest(string name0, string name1)
+        {
+            var original0 = new TwinExample1() { Name = name0 };
+            var original1 = new TwinExample0()
+            {
+                Name = name1,
+                Partner = original0,
+            };
+            original0.Partner = original1;
+
+            var binary = MessagePackSerializer.Serialize(original0, options);
+            var result = MessagePackSerializer.Deserialize<TwinExample0>(binary, options);
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Partner);
+            Assert.AreEqual(result.Name, name0);
+            Assert.AreEqual(result.Partner!.Name, name1);
+            Assert.AreSame(result.Partner.Partner, result);
+        }
+    }
+}
