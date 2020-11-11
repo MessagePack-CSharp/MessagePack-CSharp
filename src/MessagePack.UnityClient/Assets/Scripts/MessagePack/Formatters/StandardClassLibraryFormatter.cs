1b8133a3efa9bd04db2664de8fad348d258a4a4d
neuecc <ils@neue.cc>
2020-11-11 16:08:28 +0900

    remove using

--- src/MessagePack.UnityClient/Assets/Scripts/MessagePack/Formatters/StandardClassLibraryFormatter.cs
+++ src/MessagePack.UnityClient/Assets/Scripts/MessagePack/Formatters/StandardClassLibraryFormatter.cs
@@ -6,7 +6,6 @@ using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Runtime.InteropServices;
 using System.Text;
 using MessagePack.Internal;
 
