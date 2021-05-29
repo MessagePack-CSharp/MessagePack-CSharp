48bec5340b9de2a1e05a2ec457418ff3dd6c67d6
Jesse Talavera-Greenberg <jesse@jesse.tg>
2021-05-29 14:28:08 -0400

    Don't use an alias for the Odin assembly

--- benchmark/SerializerBenchmark/Serializers/OdinSerializer.cs
+++ benchmark/SerializerBenchmark/Serializers/OdinSerializer.cs
@@ -1,11 +1,9 @@
 ﻿// Copyright (c) All contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-extern alias odin;
-
 using Benchmark.Serializers;
-using odin::OdinSerializer;
-using odin::OdinSerializer.Utilities;
+using OdinSerializer;
+using OdinSerializer.Utilities;
 
 #pragma warning disable SA1649 // File name should match first type name
 
