5dc5feed0abdd423d7b8fe222b9c2a3ac96a86ae
Andrew Arnott <andrewarnott@live.com>
2021-08-21 21:19:56 -0600

    Fix unity build

--- src/MessagePack.UnityClient/Assets/Scripts/Tests/Shims/XUnit.cs
+++ src/MessagePack.UnityClient/Assets/Scripts/Tests/Shims/XUnit.cs
@@ -122,6 +122,11 @@ namespace Xunit
         {
             NUnit.Framework.Assert.AreSame(expected, actual);
         }
+
+        public static void NotSame(object expected, object actual)
+        {
+            NUnit.Framework.Assert.AreNotSame(expected, actual);
+        }
     }
 
     [Serializable]
