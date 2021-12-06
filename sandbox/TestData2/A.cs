642557b03568f8ec809da97a12c5e6239b904df3
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2021-12-06 17:55:20 +0900

    C#10 test Global Using and file scoped namespace

--- sandbox/TestData2/A.cs
+++ sandbox/TestData2/A.cs
@@ -1,17 +1,13 @@
 ﻿// Copyright (c) All contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Collections.Generic;
-using MessagePack;
-
 #pragma warning disable SA1307 // Accessible fields should begin with upper-case letter
 #pragma warning disable SA1401 // Fields should be private
 
-namespace TestData2
+namespace TestData2;
+
+[MessagePackObject(true)]
+public class A
 {
-    [MessagePackObject(true)]
-    public class A
-    {
-        public int a; public List<B> bs; public C c;
-    }
+    public int a; public List<B> bs; public C c;
 }
