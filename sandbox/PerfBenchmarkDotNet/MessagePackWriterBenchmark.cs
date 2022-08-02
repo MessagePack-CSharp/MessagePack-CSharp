MessagePack-CSharp/MessagePack-CSharp

commit 5c0220eecc34b34c76f022a46bf7c246786f0a65

Fix up build issues

sandbox/PerfBenchmarkDotNet/MessagePackWriterBenchmark.cs
@@ -4,6 +4,7 @@
 extern alias newmsgpack;
 extern alias oldmsgpack;
 
+using System;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
 using Nerdbank.Streams;
@@ -12,7 +13,7 @@ namespace PerfBenchmarkDotNet
 {
     [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
     [CategoriesColumn]
-    public class MessagePackWriterBenchmark
+    public sealed class MessagePackWriterBenchmark : IDisposable
     {
         private const int RepsOverArray = 300 * 1024;
         private readonly Sequence<byte> sequence = new Sequence<byte>();
@@ -147,6 +148,11 @@ namespace PerfBenchmarkDotNet
 
                 offset = 0;
             }
+        }
+
+        public void Dispose()
+        {
+            this.sequence.Dispose();
         }
     }
 }

commit 1092f85b68ceff6667eadf43081097e02adb77c8

Fix PerBenchmarkDotNet project
This has evidently been broken since I regressed it in 5c0220eecc34.

sandbox/PerfBenchmarkDotNet/MessagePackWriterBenchmark.cs
@@ -13,7 +13,7 @@ namespace PerfBenchmarkDotNet
 {
     [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
     [CategoriesColumn]
-    public sealed class MessagePackWriterBenchmark : IDisposable
+    public class MessagePackWriterBenchmark : IDisposable
     {
         private const int RepsOverArray = 300 * 1024;
         private readonly Sequence<byte> sequence = new Sequence<byte>();
@@ -152,7 +152,16 @@ namespace PerfBenchmarkDotNet
 
         public void Dispose()
         {
-            this.sequence.Dispose();
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.sequence.Dispose();
+            }
         }
     }
 }