07c52e288ffe6b7bbe71e22990eea272f8efc79b
Andrew Arnott <andrewarnott@live.com>
2022-12-02 12:19:32 -0700

    Add net7.0 target to mpc and updated comments

--- sandbox/TestData2/codegen.ps1
+++ sandbox/TestData2/codegen.ps1
@@ -1,1 +1,1 @@
-dotnet run -f "net6.0" --project "$PSScriptRoot/../../src/MessagePack.Generator/MessagePack.Generator.csproj" -- -i "$PSScriptRoot/TestData2.csproj" -o "$PSScriptRoot/Generated.cs"
+dotnet run -f "net7.0" --project "$PSScriptRoot/../../src/MessagePack.Generator/MessagePack.Generator.csproj" -- -i "$PSScriptRoot/TestData2.csproj" -o "$PSScriptRoot/Generated.cs"
