ceefa17010a1397ccbedd8f9c1dfcd0efd24fdf2
y-sasaki <11359025+Y-YoL@users.noreply.github.com>
2022-12-19 21:38:23 +0900

    Replace Generator

---
+++ src/MessagePack.Generator/MessagepackGenerator.Emit.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) All contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Text;
+using MessagePackCompiler.CodeAnalysis;
+using MessagePackCompiler.Generator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MessagePack.Generator;
+
+public partial class MessagepackGenerator
+{
+    private static void Generate(TypeDeclarationSyntax syntax, Compilation compilation, IGeneratorContext context)
+    {
+        var semanticModel = compilation.GetSemanticModel(syntax.SyntaxTree);
+
+        var typeSymbol = semanticModel.GetDeclaredSymbol(syntax, context.CancellationToken) as ITypeSymbol;
+        if (typeSymbol == null)
+        {
+            return;
+        }
+
+        var fullType = typeSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)
+            .Replace("global::", string.Empty)
+            .Replace("<", "_")
+            .Replace(">", "_");
+
+        var collector = new TypeCollector(compilation, true, isForceUseMap: false, ignoreTypeNames: null, typeSymbol);
+
+        var (objectInfo, enumInfo, genericInfo, unionInfo) = collector.Collect();
+
+        var code = GenerateFormatterSync(string.Empty, string.Empty, objectInfo, enumInfo, unionInfo, genericInfo);
+
+        context.AddSource($"{fullType}.MessagePackFormatter.g.cs", code);
+    }
+
+    /// <summary>
+    /// Generates the specialized resolver and formatters for the types that require serialization in a given compilation.
+    /// </summary>
+    /// <param name="resolverName">The resolver name.</param>
+    /// <param name="namespaceDot">The namespace for the generated type to be created in.</param>
+    /// <param name="objectInfo">The ObjectSerializationInfo array which TypeCollector.Collect returns.</param>
+    /// <param name="enumInfo">The EnumSerializationInfo array which TypeCollector.Collect returns.</param>
+    /// <param name="unionInfo">The UnionSerializationInfo array which TypeCollector.Collect returns.</param>
+    /// <param name="genericInfo">The GenericSerializationInfo array which TypeCollector.Collect returns.</param>
+    private static string GenerateFormatterSync(string resolverName, string namespaceDot, ObjectSerializationInfo[] objectInfo, EnumSerializationInfo[] enumInfo, UnionSerializationInfo[] unionInfo, GenericSerializationInfo[] genericInfo)
+    {
+        var objectFormatterTemplates = objectInfo
+            .GroupBy(x => (x.Namespace, x.IsStringKey))
+            .Select(x =>
+            {
+                var (nameSpace, isStringKey) = x.Key;
+                var objectSerializationInfos = x.ToArray();
+                var ns = namespaceDot + "Formatters" + (nameSpace is null ? string.Empty : "." + nameSpace);
+                var template = isStringKey ? new StringKeyFormatterTemplate(ns, objectSerializationInfos) : (IFormatterTemplate)new FormatterTemplate(ns, objectSerializationInfos);
+                return template;
+            })
+            .ToArray();
+
+        string GetNamespace<T>(IGrouping<string?, T> x)
+        {
+            if (x.Key == null)
+            {
+                return namespaceDot + "Formatters";
+            }
+
+            return namespaceDot + "Formatters." + x.Key;
+        }
+
+        var enumFormatterTemplates = enumInfo
+            .GroupBy(x => x.Namespace)
+            .Select(x => new EnumTemplate(GetNamespace(x), x.ToArray()))
+            .ToArray();
+
+        var unionFormatterTemplates = unionInfo
+            .GroupBy(x => x.Namespace)
+            .Select(x => new UnionTemplate(GetNamespace(x), x.ToArray()))
+            .ToArray();
+
+        var resolverTemplate = new ResolverTemplate(namespaceDot + "Resolvers", namespaceDot + "Formatters", resolverName, genericInfo.Where(x => !x.IsOpenGenericType).Cast<IResolverRegisterInfo>().Concat(enumInfo).Concat(unionInfo).Concat(objectInfo.Where(x => !x.IsOpenGenericType)).ToArray());
+
+        var sb = new StringBuilder();
+        sb.AppendLine(resolverTemplate.TransformText());
+        sb.AppendLine();
+        foreach (var item in enumFormatterTemplates)
+        {
+            var text = item.TransformText();
+            sb.AppendLine(text);
+        }
+
+        sb.AppendLine();
+        foreach (var item in unionFormatterTemplates)
+        {
+            var text = item.TransformText();
+            sb.AppendLine(text);
+        }
+
+        sb.AppendLine();
+        foreach (var item in objectFormatterTemplates)
+        {
+            var text = item.TransformText();
+            sb.AppendLine(text);
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ src/MessagePack.Generator/MessagepackGenerator.cs
@@ -0,0 +1,128 @@
+﻿// Copyright (c) All contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MessagePack.Generator;
+
+[Generator(LanguageNames.CSharp)]
+public partial class MessagepackGenerator : IIncrementalGenerator
+{
+    public const string MessagePackObjectAttributeFullName = "MessagePack.MessagePackObjectAttribute";
+
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var typeDeclarations = context.SyntaxProvider.ForAttributeWithMetadataName(
+            MessagePackObjectAttributeFullName,
+            predicate: static (node, _) => node is TypeDeclarationSyntax,
+            transform: static (context, _) => (TypeDeclarationSyntax)context.TargetNode);
+
+        var source = typeDeclarations
+            .Combine(context.CompilationProvider)
+            .WithComparer(Comparer.Instance);
+
+        context.RegisterSourceOutput(source, static (context, source) =>
+        {
+            var (typeDeclaration, compilation) = source;
+            Generate(typeDeclaration, compilation, new GeneratorContext(context));
+        });
+    }
+
+    private class Comparer : IEqualityComparer<(TypeDeclarationSyntax, Compilation)>
+    {
+        public static readonly Comparer Instance = new Comparer();
+
+        public bool Equals((TypeDeclarationSyntax, Compilation) x, (TypeDeclarationSyntax, Compilation) y)
+        {
+            return x.Item1.Equals(y.Item1);
+        }
+
+        public int GetHashCode((TypeDeclarationSyntax, Compilation) obj)
+        {
+            return obj.Item1.GetHashCode();
+        }
+    }
+
+    private class GeneratorContext : IGeneratorContext
+    {
+        private SourceProductionContext context;
+
+        public GeneratorContext(SourceProductionContext context)
+        {
+            this.context = context;
+        }
+
+        public CancellationToken CancellationToken => context.CancellationToken;
+
+        public void AddSource(string hintName, string source) => context.AddSource(hintName, source);
+    }
+
+#if false
+    public async Task RunAsync(
+        [Option("i", "Input path to MSBuild project file or the directory containing Unity source files.")] string input,
+        [Option("o", "Output file path(.cs) or directory (multiple generate file).")] string output,
+        [Option("c", "Conditional compiler symbols, split with ','. Ignored if a project file is specified for input.")] string? conditionalSymbol = null,
+        [Option("r", "Set resolver name.")] string resolverName = "GeneratedResolver",
+        [Option("n", "Set namespace root name.")] string @namespace = "MessagePack",
+        [Option("m", "Force use map mode serialization.")] bool useMapMode = false,
+        [Option("ms", "Generate #if-- files by symbols, split with ','.")] string? multipleIfDirectiveOutputSymbols = null,
+        [Option("ei", "Ignore type names.")] string[]? externalIgnoreTypeNames = null)
+    {
+        try
+        {
+            Compilation compilation;
+            if (Directory.Exists(input))
+            {
+                string[]? conditionalSymbols = conditionalSymbol?.Split(',');
+                compilation = await PseudoCompilation.CreateFromDirectoryAsync(input, conditionalSymbols, this.Context.CancellationToken);
+            }
+            else
+            {
+                (workspace, compilation) = await this.OpenMSBuildProjectAsync(input, this.Context.CancellationToken);
+            }
+
+            await new MessagePackCompiler.CodeGenerator(x => Console.WriteLine(x), this.Context.CancellationToken)
+                .GenerateFileAsync(
+                    compilation,
+                    output,
+                    resolverName,
+                    @namespace,
+                    useMapMode,
+                    multipleIfDirectiveOutputSymbols,
+                    externalIgnoreTypeNames).ConfigureAwait(false);
+        }
+        catch (OperationCanceledException)
+        {
+            await Console.Error.WriteLineAsync("Canceled");
+            throw;
+        }
+        finally
+        {
+            workspace?.Dispose();
+        }
+    }
+
+    private async Task<(Workspace Workspace, Compilation Compilation)> OpenMSBuildProjectAsync(string projectPath, CancellationToken cancellationToken)
+    {
+        var workspace = MSBuildWorkspace.Create();
+        try
+        {
+            var logger = new ConsoleLogger(Microsoft.Build.Framework.LoggerVerbosity.Quiet);
+            var project = await workspace.OpenProjectAsync(projectPath, logger, null, cancellationToken);
+            var compilation = await project.GetCompilationAsync(cancellationToken);
+            if (compilation is null)
+            {
+                throw new NotSupportedException("The project does not support creating Compilation.");
+            }
+
+            return (workspace, compilation);
+        }
+        catch
+        {
+            workspace.Dispose();
+            throw;
+        }
+    }
+#endif
+}
