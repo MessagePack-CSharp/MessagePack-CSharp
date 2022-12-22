4db43421949fe26f779347b6ce1c20f1a3af9406
y-sasaki <11359025+Y-YoL@users.noreply.github.com>
2022-12-22 21:40:26 +0900

    Add Generator.Roslyn3 csproj

---
+++ src/MessagePack.Generator.Roslyn3/MessagepackGenerator.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) All contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MessagePack.Generator;
+
+[Generator(LanguageNames.CSharp)]
+public partial class MessagepackGenerator : ISourceGenerator
+{
+    public const string MessagePackObjectAttributeFullName = "MessagePack.MessagePackObjectAttribute";
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        context.RegisterForSyntaxNotifications(SyntaxContextReceiver.Create);
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        if (context.SyntaxContextReceiver is not SyntaxContextReceiver receiver || receiver.ClassDeclarations.Count == 0)
+        {
+            return;
+        }
+
+        var compiation = context.Compilation;
+        var generateContext = new GeneratorContext(context);
+
+        foreach (var syntax in receiver.ClassDeclarations)
+        {
+            Generate(syntax, compiation, generateContext);
+        }
+    }
+
+    private class SyntaxContextReceiver : ISyntaxContextReceiver
+    {
+        internal static ISyntaxContextReceiver Create()
+        {
+            return new SyntaxContextReceiver();
+        }
+
+        public HashSet<TypeDeclarationSyntax> ClassDeclarations { get; } = new();
+
+        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+        {
+            if (context.Node is TypeDeclarationSyntax typeSyntax)
+            {
+                if (typeSyntax.AttributeLists.Count > 0)
+                {
+                    var hasAttribute = typeSyntax.AttributeLists
+                        .SelectMany(x => x.Attributes)
+                        .Any(x => x.Name.ToString() is "MessagePackObject"
+                            or "MessagePackObjectAttribute"
+                            or "MessagePack.MessagePackObject"
+                            or "MessagePack.MessagePackObjectAttribute"
+                            or "Union"
+                            or "UnionAttribute"
+                            or "MessagePack.Union"
+                            or "MessagePack.UnionAttribute");
+                    if (hasAttribute)
+                    {
+                        ClassDeclarations.Add(typeSyntax);
+                    }
+                }
+            }
+        }
+    }
+
+    private class GeneratorContext : IGeneratorContext
+    {
+        private GeneratorExecutionContext context;
+
+        public GeneratorContext(GeneratorExecutionContext context)
+        {
+            this.context = context;
+        }
+
+        public CancellationToken CancellationToken => context.CancellationToken;
+
+        public void AddSource(string hintName, string source) => context.AddSource(hintName, source);
+    }
+}
