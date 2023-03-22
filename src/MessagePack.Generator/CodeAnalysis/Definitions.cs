3c2794634b172d7c4eeeae2ed431492a7dace5be
Andrew Arnott <andrewarnott@live.com>
2023-03-22 10:09:35 -0600

    Break up the outputs into individual files
    This will help with cacheability.

--- src/MessagePack.Generator/CodeAnalysis/Definitions.cs
+++ src/MessagePack.Generator/CodeAnalysis/Definitions.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) All contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using MessagePack.Generator.Transforms;
 
 #pragma warning disable SA1402 // File may only contain a single type
