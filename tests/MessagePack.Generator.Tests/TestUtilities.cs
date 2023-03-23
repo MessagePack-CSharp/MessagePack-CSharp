f8f78e718d08ed489a460f6b1f42d22db754dd93
Andrew Arnott <andrewarnott@live.com>
2023-03-22 20:13:57 -0600

    Delete or restore old test code

--- tests/MessagePack.Generator.Tests/TestUtilities.cs
+++ tests/MessagePack.Generator.Tests/TestUtilities.cs
@@ -2,21 +2,26 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Reflection;
+using Microsoft;
 
-namespace MessagePack.Generator.Tests
+namespace MessagePack.Generator.Tests;
+
+internal static class TestUtilities
 {
-    internal static class TestUtilities
+    /// <summary>
+    /// Fetches the static instance of the named resolver, by its <see langword="public" /> <see langword="static"/> <c>Instance</c> property.
+    /// </summary>
+    /// <param name="assembly">The assembly to retrieve the resolver from.</param>
+    /// <param name="name">The full name of the resolver.</param>
+    /// <returns>The resolver.</returns>
+    internal static IFormatterResolver GetResolverInstance(Assembly assembly, string name)
     {
-        /// <summary>
-        /// Fetches the static instance of the named resolver, by its <see langword="public" /> <see langword="static"/> <c>Instance</c> property.
-        /// </summary>
-        /// <param name="assembly">The assembly to retrieve the resolver from.</param>
-        /// <param name="name">The full name of the resolver.</param>
-        /// <returns>The resolver.</returns>
-        internal static IFormatterResolver GetResolverInstance(Assembly assembly, string name)
-        {
-            var resolverType = assembly.GetType(name);
-            return (IFormatterResolver)resolverType.GetField("Instance", BindingFlags.Static | BindingFlags.Public).GetValue(null);
-        }
+        Type? resolverType = assembly.GetType(name);
+        Requires.Argument(resolverType is not null, nameof(name), "No type with the given name found.");
+        FieldInfo? instanceField = resolverType.GetField("Instance", BindingFlags.Static | BindingFlags.Public);
+        Assert.NotNull(instanceField);
+        object? instanceValue = instanceField.GetValue(null);
+        Assert.NotNull(instanceValue);
+        return (IFormatterResolver)instanceValue;
     }
 }
