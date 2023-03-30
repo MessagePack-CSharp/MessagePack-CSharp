318d8a7870dbe306e4cb45782e71dd7d4832fed9
Andrew Arnott <andrewarnott@live.com>
2023-03-30 12:59:26 -0600

    Break out test classes into their own files

---
+++ tests/MessagePack.Generator.ExecutionTests/HasPropertiesWithGetterAndCtor.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) All contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+[MessagePackObject(false)]
+internal record HasPropertiesWithGetterAndCtor
+{
+    [Key(0)]
+    internal int A { get; }
+
+    [Key(1)]
+    internal string? B { get; }
+
+    internal HasPropertiesWithGetterAndCtor(int a, string? b)
+    {
+        A = a;
+        B = b;
+    }
+}
