6cfb8f4ead60d515168e8fc5fe19dabfde89fc05
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2024-01-25 02:11:50 +0900

    Fix bugs

--- src/MessagePack.Experimental/HardwareIntrinsics/BooleanArrayFormatter.cs
+++ src/MessagePack.Experimental/HardwareIntrinsics/BooleanArrayFormatter.cs
@@ -27,7 +27,7 @@ public sealed unsafe partial class BooleanArrayFormatter
         var rawSequence = reader.ReadRaw(len);
         var array = new bool[len];
         ref var outputIterator = ref MemoryMarshal.GetArrayDataReference(array);
-        nint outputOffset = 0;
+        nuint outputOffset = 0;
         foreach (var memory in rawSequence)
         {
             reader.CancellationToken.ThrowIfCancellationRequested();
@@ -37,51 +37,56 @@ public sealed unsafe partial class BooleanArrayFormatter
             }
 
             ref var inputIterator = ref MemoryMarshal.GetReference(memory.Span);
-            var inputLength = memory.Length;
-            if (Vector128.IsHardwareAccelerated)
+            var inputLength = (nuint)memory.Length;
+            if (inputLength > (nuint)array.Length - outputOffset)
             {
-                const int mask = ~15;
-                var alignedInputLength = (nuint)(inputLength & mask);
-                if (alignedInputLength > 0)
-                {
-                    for (nuint inputOffset = 0; inputOffset < alignedInputLength; inputOffset += (nuint)Vector256<byte>.Count)
-                    {
-                        reader.CancellationToken.ThrowIfCancellationRequested();
-
-                        var loaded = Vector128.LoadUnsafe(ref inputIterator, inputOffset).AsSByte();
-                        var results = loaded - Vector128.Create(unchecked((sbyte)MessagePackCode.False));
-                        var error = Vector128.ExtractMostSignificantBits(Vector128.GreaterThan(results, Vector128.Create((sbyte)1)) | Vector128.LessThan(results, Vector128<sbyte>.Zero));
-                        if (error == 0)
-                        {
-                            results.StoreUnsafe(ref Unsafe.As<bool, sbyte>(ref Unsafe.AddByteOffset(ref outputIterator, inputOffset + (nuint)outputOffset)));
-                        }
-                        else
-                        {
-                            ThrowUnexpectedMessagePackCode(Unsafe.AddByteOffset(ref inputIterator, inputOffset + (nuint)BitOperations.TrailingZeroCount(error)));
-                        }
-                    }
-                }
-
-                outputOffset = (nint)alignedInputLength;
+                inputLength = (nuint)(nint)array.Length - outputOffset;
             }
 
-            for (nint i = 0; i < inputLength; i++)
+            nuint inputOffset = 0;
+            if (Vector128.IsHardwareAccelerated)
+            {
+                const nuint mask = ~15U;
+                var alignedInputLength = inputLength & mask;
+                for (; inputOffset < alignedInputLength; inputOffset += (nuint)Vector128<byte>.Count)
+                {
+                    reader.CancellationToken.ThrowIfCancellationRequested();
+
+                    var loaded = Vector128.LoadUnsafe(ref inputIterator, inputOffset).AsSByte();
+
+                    // MessagePackCode.True is 0xc3(-61). MessagePackCode.False is 0xc2(-62).
+                    var results = loaded - Vector128.Create(unchecked((sbyte)MessagePackCode.False));
+                    var error = Vector128.ExtractMostSignificantBits(Vector128.GreaterThan(results, Vector128<sbyte>.One) | Vector128.LessThan(results, Vector128<sbyte>.Zero));
+                    if (error == 0)
+                    {
+                        results.StoreUnsafe(ref Unsafe.As<bool, sbyte>(ref Unsafe.AddByteOffset(ref outputIterator, inputOffset + outputOffset)));
+                    }
+                    else
+                    {
+                        ThrowUnexpectedMessagePackCode(Unsafe.AddByteOffset(ref inputIterator, inputOffset + outputOffset + (nuint)BitOperations.TrailingZeroCount(error)));
+                    }
+                }
+            }
+
+            for (; inputOffset < inputLength; inputOffset++)
             {
                 reader.CancellationToken.ThrowIfCancellationRequested();
-                var code = Unsafe.AddByteOffset(ref inputIterator, i);
+                var code = Unsafe.AddByteOffset(ref inputIterator, inputOffset);
                 switch (code)
                 {
                     case MessagePackCode.True:
-                        Unsafe.AddByteOffset(ref outputIterator, outputOffset++) = true;
+                        Unsafe.AddByteOffset(ref outputIterator, inputOffset + outputOffset) = true;
                         break;
                     case MessagePackCode.False:
-                        Unsafe.AddByteOffset(ref outputIterator, outputOffset++) = false;
+                        Unsafe.AddByteOffset(ref outputIterator, inputOffset + outputOffset) = false;
                         break;
                     default:
                         ThrowUnexpectedMessagePackCode(code);
                         break;
                 }
             }
+
+            outputOffset += inputLength;
         }
 
         return array;
