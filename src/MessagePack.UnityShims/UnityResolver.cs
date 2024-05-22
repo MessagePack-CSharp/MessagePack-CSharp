035b4138225dd813deaf1762d8379e8f916974a7
Andrew Arnott <andrewarnott@live.com>
2024-05-22 15:03:14 -0600

    Fix build break in UnityShims

--- src/MessagePack.UnityShims/UnityResolver.cs
+++ src/MessagePack.UnityShims/UnityResolver.cs
@@ -1,4 +1,4 @@
-﻿// Cropyright (c) All contributors. All rights reserved.
+﻿// Copyright (c) All contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #nullable enable
