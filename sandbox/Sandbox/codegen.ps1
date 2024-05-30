873227277c82e9fdb7e4f63df728b51fc7ac1453
Andrew Arnott <andrewarnott@live.com>
2024-05-29 17:46:01 -0700

    Add .NET 8 target to mpc
    Also drop .NET 7 target, since .NET 7 is no longer supported by Microsoft.
    
    Fixes #1831

--- sandbox/Sandbox/codegen.ps1
+++ sandbox/Sandbox/codegen.ps1
@@ -1,1 +1,1 @@
-dotnet run -f "net7.0" --project "$PSScriptRoot/../../src/MessagePack.Generator/MessagePack.Generator.csproj" -- -i "$PSScriptRoot/../SharedData/SharedData.csproj" -o "$PSScriptRoot/Generated.cs"
+dotnet run -f net8.0 --project "$PSScriptRoot/../../src/MessagePack.Generator/MessagePack.Generator.csproj" -- -i "$PSScriptRoot/../SharedData/SharedData.csproj" -o "$PSScriptRoot/Generated.cs"
