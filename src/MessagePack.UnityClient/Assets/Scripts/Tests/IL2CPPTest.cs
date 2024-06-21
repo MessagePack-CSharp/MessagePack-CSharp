c5ba1ca3fac407021b922804d808fe4972a675c6
neuecc <ils@neue.cc>
2024-06-21 16:41:30 +0900

    for Unity

---
+++ src/MessagePack.UnityClient/Assets/Scripts/Tests/IL2CPPTest.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+
+using MessagePack;
+using NUnit.Framework;
+
+namespace Assets.Scripts.Tests
+{
+    public class IL2CPPTest
+    {
+        [Test]
+        public void SimpleSerializeAndDeserialize()
+        {
+            var mc = new MyClass() { Age = 99, Name = "foo" };
+            var bin = MessagePackSerializer.Serialize(mc);
+
+            var canGetFormatter = GeneratedMessagePackResolver.Instance.GetFormatter<MyClass>();
+            Assert.AreNotEqual(canGetFormatter, null);
+
+            var mc2 = MessagePackSerializer.Deserialize<MyClass>(bin);
+
+            Assert.AreEqual(mc.Age, mc2.Age);
+            Assert.AreEqual(mc.Name, mc2.Name);
+        }
+    }
+
+    [MessagePackObject]
+    public class MyClass
+    {
+        [Key(0)]
+        public int Age { get; set; }
+
+        [Key(1)]
+        public string? Name { get; set; }
+    }
+}
