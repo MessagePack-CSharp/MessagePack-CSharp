ab22ce8c629ebd8e8ad19ba3019e9d5fd6a3bfba
Andrew Arnott <andrew.arnott@microsoft.com>
2024-10-11 12:46:57 -0600

    Assert that library is AOT friendly

---
+++ tests/NativeAOTCompatibility/Program.cs
@@ -0,0 +1,4 @@
+// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+System.Console.WriteLine("This test is run by \"dotnet publish -c release -r [RID]-x64\" rather than by executing the program.");
