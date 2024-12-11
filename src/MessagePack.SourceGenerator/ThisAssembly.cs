acb800355f2dc5cbae0d331a5bbfd71b5035e4a3
neuecc <ils@neue.cc>
2024-12-11 12:47:02 +0900

    p

--- src/MessagePack.SourceGenerator/ThisAssembly.cs
+++ src/MessagePack.SourceGenerator/ThisAssembly.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace MessagePack.SourceGenerator
 {
-    internal static class ThisAssembly
+    public static class ThisAssembly
     {
         public const string AssemblyFileVersion = "1.0.0";
     }
