1b9432566e4539e728b1d8d3f1b0b27dd0eb3cff
neuecc <ils@neue.cc>
2024-12-11 12:42:36 +0900

    TODO: need  modify ThisAssembly.AssemblyFileVersion

---
+++ src/MessagePack.SourceGenerator/ThisAssembly.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessagePack.SourceGenerator
+{
+    internal static class ThisAssembly
+    {
+        public const string AssemblyFileVersion = "1.0.0";
+    }
+}
