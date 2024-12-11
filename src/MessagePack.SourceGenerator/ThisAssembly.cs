b84eadd2c950082836c62780d33f9e9cca05c807
neuecc <ils@neue.cc>
2024-12-11 15:09:32 +0900

    versiontest

--- src/MessagePack.SourceGenerator/ThisAssembly.cs
+++ src/MessagePack.SourceGenerator/ThisAssembly.cs
@@ -1,4 +1,7 @@
-﻿namespace MessagePack.SourceGenerator
+﻿// Copyright (c) All contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace MessagePack.SourceGenerator
 {
     public static class ThisAssembly
     {
@@ -6,7 +9,17 @@
         {
             get
             {
-                return typeof(ThisAssembly).Assembly.GetName().Version.ToString();
+                var version = typeof(ThisAssembly).Assembly.GetName().Version.ToString();
+                return version;
+            }
+        }
+
+        public static Version Version
+        {
+            get
+            {
+                var version = typeof(ThisAssembly).Assembly.GetName().Version;
+                return version;
             }
         }
     }
